0265d832707ad939852fc90f5a6fe9ad6665da2a
Lory <147001347+LaurianeH-05@users.noreply.github.com>
2024-05-07 22:41:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-# main.py
 
-def main():
-    print("Hello, welcome to our game!")
-    print("We will bring you something exciting, soon!")
-    print("Enjoy your adventure!")
-    print("Conflicting feature in welcome-feature branch")
-    print("Please choose your player.")
-
-if __name__ == "__main__":
-    main()
